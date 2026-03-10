ed5bcd79c28e6b392b054190b2148e228947692b
ar-eeb1 <areebamir.pk@gmail.com>
2026-03-10 06:23:30 +0500

    rotation

--- components/LayoutContent.jsx
+++ components/LayoutContent.jsx
@@ -12,7 +12,7 @@ export default function LayoutContent({ children }) {
         <div className="flex min-h-screen">
             {/* Left Vertical Ad Section */}
             <aside className="lg:w-32 md:w-16 w-13 bg-emerald-900 sticky top-0 h-screen flex items-center justify-center">
-                <button onClick={() => setOpen(true)} className="bg-emerald-500 rounded-full px-2 rotate-90 min-w-56 text-sm text-white cursor-pointer ">
+                <button onClick={() => setOpen(true)} className="bg-emerald-500 rounded-full px-2 rotate-270 min-w-56 text-sm text-white cursor-pointer ">
                     Place your ad here!
                 </button>
             </aside>
